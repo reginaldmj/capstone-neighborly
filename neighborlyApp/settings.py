7d264191b5caa1286f55055b745b35717b7d809a
Tommy Bello <tbello4299@gmail.com>
2021-10-04 14:10:01 -0400

    added .env and moved Secret into it

--- neighborlyApp/settings.py
+++ neighborlyApp/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--#6&8&(%m@u#a(uorh9-g$%l(5z*!t46cqk74$2%0um7#%!ga@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
