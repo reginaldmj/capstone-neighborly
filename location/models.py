97c54a10746c0913cbeefb6ce09bdfd58614b40c
Tommy Bello <tbello4299@gmail.com>
2021-10-19 12:31:31 -0400

    small bug fixes and added a site description on the login page

--- location/models.py
+++ location/models.py
@@ -2,17 +2,15 @@
 from django.db import models
 
 
-
-
 # Create your models here.
 
 
 class Neighborhood(models.Model):
-    name = models.CharField(max_length=150, blank=True, null=True )
-    address = models.CharField(max_length=150, blank=True, null=True )
+    name = models.CharField(max_length=150, blank=True, null=True)
+    address = models.CharField(max_length=150, blank=True, null=True)
     city = models.CharField(max_length=50, blank=True, null=True)
     state = models.CharField(max_length=50, blank=True, null=True)
-    zipcode = models.IntegerField(default=0)
+    zipcode = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
